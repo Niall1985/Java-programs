fbcbcb04c153b70418e28bc27d178cc0a34c6831
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2024-11-01 10:59:32 +0530

    Update FileIO.java

--- FileIO.java
+++ FileIO.java
@@ -98,3 +98,76 @@ public class Main{
         
     }
 }
+
+
+
+
+// Single File Programming Question
+// Problem Statement
+
+
+
+// Cindy is working on a text processing program, and she needs your help. The program is designed to replace all vowels in a given text with a specified consonant. 
+
+
+
+// Create a file named file1.txt and prompt the user to input a text. Write the user's input into file1.txt. Then, read the content and perform a specific logic (replacing vowels with a consonant provided by the user), and write the modified result into a new file named file2.txt. Lastly, display the modified result obtained from file2.txt. 
+
+// Input format :
+// The first line of input consists of a string, representing the text that needs vowel replacement.
+
+// The second line is a single character value, representing the consonant to replace the vowels with.
+
+// Output format :
+// The output displays the string representing the modified text after replacing the vowels with the specified consonant.
+
+
+
+// Refer to the sample output for the formatting specifications.
+
+// Code constraints :
+// The string contains lowercase and uppercase with at most 80 characters.
+
+// Sample test cases :
+// Input 1 :
+// Hello World
+// X
+// Output 1 :
+// HXllX WXrld
+// Input 2 :
+// Programming is fun
+// y
+// Output 2 :
+// Prygrymmyng ys fyn
+// Input 3 :
+// Apple
+// r
+// Output 3 :
+// rpplr
+
+
+
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String sent = s.nextLine();
+        char character = s.next().charAt(0);
+        StringBuilder modifiedString = new StringBuilder();
+        for(int i = 0 ; i < sent.length() ; i++){
+            
+            char ch = sent.charAt(i);
+            if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || 
+            ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                modifiedString.append(character);
+            }
+            else{
+                modifiedString.append(ch);
+            }
+        }
+        System.out.printf("%s", modifiedString);
+    }
+}
