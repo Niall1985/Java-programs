384957bccdd17d284b6ea1b19a6dec956854aa45
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2024-11-14 16:39:16 +0530

    Update Array_Of_Objects.java

--- Array_Of_Objects.java
+++ Array_Of_Objects.java
@@ -33,3 +33,85 @@ public class Main{
     }
   }
 }
+
+
+
+
+
+
+
+
+
+import java.util.Scanner;
+
+class ClothingItem{
+    String name;
+    double price;
+    int quantity;
+    
+    ClothingItem(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    double getPrice(){
+        return price;
+    }
+    
+    int getQuantity(){
+        return quantity;
+    }
+    
+    public static double calculateGrossWorth(ClothingItem[] items){
+        double grossWorth = 0;
+        for(ClothingItem item : items){
+            grossWorth += item.getPrice() * item.getQuantity();
+        }
+        return grossWorth;
+    }
+    
+    public static double calculateNetWorth(ClothingItem[] items){
+        double netWorth = 0;
+        for(ClothingItem item : items){
+            double val = item.getPrice() * item.getQuantity();
+            double discount = 0;
+            if(val > 50.00){
+                discount = val*0.10;
+            }
+            else if(item.getQuantity() > 10){
+                discount = val*0.15;
+            }
+            else if(val <= 50.00 && item.getQuantity() > 5){
+                discount = val*0.05;
+            }
+            netWorth += val - discount;
+        }
+        return netWorth;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfItems = scanner.nextInt();
+
+        ClothingItem[] items = new ClothingItem[numberOfItems];
+
+        for (int i = 0; i < numberOfItems; i++) {
+            String name = scanner.nextLine();
+            scanner.nextLine();
+            double price = scanner.nextDouble();
+            int quantity = scanner.nextInt();
+
+            items[i] = new ClothingItem(name, price, quantity);
+        }
+
+        double grossWorth = ClothingItem.calculateGrossWorth(items);
+        double netWorth = ClothingItem.calculateNetWorth(items);
+
+        System.out.printf("%.2f%n", grossWorth);
+        System.out.printf("%.2f", netWorth);
+    }
+}
