45225893735e4ff0f5bbb68b10b09f89b024cb37
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2026-01-12 20:50:24 +0530

    Update Vernam.java

--- Vernam.java
+++ Vernam.java
@@ -12,16 +12,33 @@ public class Main{
       }
       cipher[i] = alphakey.charAt(res);
     }
-    // System.out.println("Encrypted: " + new String(cipher));
     String ciphertext = new String(cipher);
     return ciphertext;
   }
   
+  static String decrpyt(String ciphertext, String key, String alphakey){
+    char[] original = new char[ciphertext.length()];
+    for(int i = 0 ; i < ciphertext.length() ; i++){
+      int indexP = alphakey.indexOf(ciphertext.charAt(i));
+      int indexK = alphakey.indexOf(key.charAt(i));
+      int res = indexP ^ indexK;
+      if(res > 26){
+        res -= 26;
+      }
+      original[i] = alphakey.charAt(res);
+    }
+    String originaltext = new String(original);
+    return originaltext;
+  }
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     String plaintext = sc.next();
     String key = sc.next();
     String alphakey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     String ciphertext = encrpyt(plaintext, key, alphakey);
+    System.out.println("Encrypted: " + ciphertext);
+    
+    String original = decrpyt(ciphertext, key, alphakey);
+    System.out.println("Decrypted: " + original);
   }
 }
