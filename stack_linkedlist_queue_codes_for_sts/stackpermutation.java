ba473b4d057c73051a38a06a62ed8fe88a778774
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2026-01-29 12:00:27 +0530

    Create stackpermutation.java

---
+++ stack_linkedlist_queue_codes_for_sts/stackpermutation.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main{
+  static boolean isPermutable(int[]original, int[]target){
+    Stack<Integer>stack = new Stack<>();
+    int i = 0;
+    for(int element : original){
+      stack.push(element);
+      while(!stack.isEmpty() && stack.peek() == target[i]){
+        stack.pop();
+        i++;
+      }
+    }
+    return stack.isEmpty();
+  }
+  public static void main(String[] args) {
+    int[]original = {1,2,3};
+    int[]target = {2,1,3};
+    
+    boolean result = isPermutable(original, target);
+    if(result){
+      System.out.println("Yes");
+    }
+    else{
+      System.out.println("No");
+    }
+  }
+}
