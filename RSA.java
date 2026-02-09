41653f1ed3abcbf13379f22bc8a1197b57e1c9b4
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2026-02-09 09:03:00 +0530

    Update RSA.java

--- RSA.java
+++ RSA.java
@@ -1,47 +1,52 @@
 import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
-  public static int GCD(int a, int b){
-    while(b != 0){
-      int temp = b;
-      b = a % b;
-      a = temp;
+    public static int GCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
-    return a;
-  }
+
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int P = sc.nextInt();
-      int Q = sc.nextInt();
-      int N = P*Q;
-      int plain = sc.nextInt();
-      int totient = (P-1) * (Q-1);
-      int e = 0;
-      
-      for(int i = 2 ; i <= totient ; i++){
-        int gcdval = GCD(i, totient);
-        if(gcdval == 1){
-          e = i;
-          break;
+        Scanner sc = new Scanner(System.in);
+        int P = sc.nextInt();
+        int Q = sc.nextInt();
+        BigInteger N = BigInteger.valueOf(P).multiply(BigInteger.valueOf(Q));
+        int plain = sc.nextInt();
+        int totient = (P - 1) * (Q - 1);
+        System.out.println("Totient: " + totient);
+        int e = 0;
+
+        for (int i = 2; i <= totient; i++) {
+            int gcdval = GCD(i, totient);
+            if (gcdval == 1) {
+                e = i;
+                break;
+            }
         }
-      }
-      System.out.println("(e, n): (" + e + ", " + N + ")");
-      
-      int d = 0;
-      for(int i = 2 ; i < totient ; i++){
-        if(((e * i) / totient) == 1){
-          d = i;
-          break;
+        System.out.println("(e, n): (" + e + ", " + N + ")");
+
+        int d = 0;
+        for (int i = 2; i < totient; i++) {
+            if (((e * i) % totient) == 1) {
+                d = i;
+                break;
+            }
         }
-      }
-      System.out.println("(d, n): (" + d + ", " + N + ")");
-      
-      int cipher = (int)Math.pow(plain, e);
-      cipher = cipher % totient;
-      System.out.println("Cipher: " + cipher);
-      
-      int plaintext = (int)Math.pow(cipher, d);
-      plaintext = plaintext % N;
-      System.out.println("Plaintext: " + plaintext);
+        System.out.println("(d, n): (" + d + ", " + N + ")");
+
+        BigInteger bigPlain = BigInteger.valueOf(plain);
+        BigInteger bigE = BigInteger.valueOf(e);
+        BigInteger cipher = bigPlain.modPow(bigE, N);
+        System.out.println("Cipher: " + cipher);
+
+        BigInteger bigD = BigInteger.valueOf(d);
+        BigInteger decrypted = cipher.modPow(bigD, N);
+        int plaintext = decrypted.intValue();
+        System.out.println("Plaintext: " + plaintext);
     }
 }
