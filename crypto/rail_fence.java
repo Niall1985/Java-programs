da962d127735cddb691861b292acc92f9cfa5ee5
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2026-02-22 09:31:12 +0530

    Update rail_fence.java

--- crypto/rail_fence.java
+++ crypto/rail_fence.java
@@ -1,103 +1,91 @@
-import java.util.*; 
- 
-public class Main { 
- 
-    static String encrypt(String text, int key) { 
-        int len = text.length(); 
-        char[][] rail = new char[key][len]; 
- 
-        for (int i = 0; i < key; i++) { 
-            Arrays.fill(rail[i], '.'); 
-        } 
- 
-        int row = 0; 
-        boolean movingDown = false; 
- 
-        for (int i = 0; i < len; i++) { 
-            rail[row][i] = text.charAt(i); 
- 
-            if (row == 0 || row == key - 1) { 
-                movingDown = !movingDown; 
-            } 
-            row += movingDown ? 1 : -1; 
-        } 
- 
-        System.out.println("Encryption Matrix:"); 
-        for (int i = 0; i < key; i++) { 
-            for (int j = 0; j < len; j++) { 
-                System.out.print(rail[i][j] + " "); 
-            }
-            
-            System.out.println(); 
-        } 
- 
-        StringBuilder cipher = new StringBuilder(); 
-        for (int i = 0; i < key; i++) { 
-            for (int j = 0; j < len; j++) { 
-                if (rail[i][j] != '.') { 
-                    cipher.append(rail[i][j]); 
-                } 
-            } 
-        } 
-        return cipher.toString(); 
+import java.util.*;
+public class Main{
+  static String encrypt(String text, int key){
+    char [][]rail = new char[key][text.length()];
+    char []textarr = text.toCharArray();
+    for(int i = 0 ; i < key ; i++){
+      Arrays.fill(rail[i], '.');
+    }
+    int row = 0;
+    boolean movedown = false;
+    for(int i = 0 ; i < text.length() ; i++){
+      rail[row][i] = textarr[i];
+      if(row == 0 || row == key - 1){
+        movedown = !movedown;
+      }
+      row += movedown ? 1 : -1;
+    }
+    System.out.println("Encryption Matrix:"); 
+    for (int i = 0; i < key; i++) { 
+      for (int j = 0; j < text.length(); j++) { 
+        System.out.print(rail[i][j] + " "); 
+      }
+      System.out.println(); 
     } 
- 
-    static String decrypt(String cipher, int key) { 
-        int len = cipher.length(); 
-        char[][] rail = new char[key][len]; 
-        for (int i = 0; i < key; i++) { 
-            Arrays.fill(rail[i], '.'); 
+    
+    StringBuilder cipherstring = new StringBuilder();
+    for(int i = 0 ; i < key ; i++){
+      for(int j = 0 ; j < text.length() ; j++){
+        if(rail[i][j] != '.'){
+          cipherstring.append(rail[i][j]);
+        }
+      }
+    }
+    String ciphertext = cipherstring.toString();
+    return ciphertext;
+  }
+  
+  static String decrypt(String ciphertext, int key){
+    char[][]rail = new char[key][ciphertext.length()];
+    for(int i = 0 ; i < key ; i++){
+      Arrays.fill(rail[i], '.');
+    }
+    int row = 0;
+    boolean movedown = false;
+    for(int i = 0 ; i < ciphertext.length() ; i++){
+      rail[row][i] = '*';
+      if(row == 0 || row == key - 1){
+        movedown = !movedown;
+      }
+      row += movedown ? 1 : -1;
+    }
+    
+    int index = 0;
+    for(int i = 0 ; i < key ; i++){
+      for(int j = 0 ; j < ciphertext.length() ; j++){
+        if(rail[i][j] == '*' && index < ciphertext.length()){
+          rail[i][j] = ciphertext.charAt(index++);
+        }
+      }
+    }
+    System.out.println("\nDecryption Matrix:"); 
+    for (int i = 0; i < key; i++) { 
+        for (int j = 0; j < ciphertext.length(); j++) { 
+            System.out.print(rail[i][j] + " "); 
         } 
-        boolean movingDown = false; 
-        int row = 0; 
-        for (int i = 0; i < len; i++) { 
-            rail[row][i] = '*'; 
- 
-            if (row == 0 || row == key - 1) { 
-                movingDown = !movingDown; 
-            } 
-            row += movingDown ? 1 : -1; 
-        } 
-        int index = 0; 
-        for (int i = 0; i < key; i++) { 
-            for (int j = 0; j < len; j++) { 
-                if (rail[i][j] == '*' && index < len) { 
-                    rail[i][j] = cipher.charAt(index++); 
-                } 
-            } 
-        } 
-        
-        System.out.println("Decryption Matrix:"); 
-        for (int i = 0; i < key; i++) { 
-            for (int j = 0; j < len; j++) { 
-                System.out.print(rail[i][j] + " "); 
-            } 
-            System.out.println(); 
-        } 
-        StringBuilder plain = new StringBuilder(); 
-        movingDown = false; 
-        row = 0; 
- 
-        for (int i = 0; i < len; i++) { 
-            plain.append(rail[row][i]); 
- 
-            if (row == 0 || row == key - 1) { 
-                movingDown = !movingDown; 
-            } 
-            row += movingDown ? 1 : -1; 
-        } 
-        return plain.toString(); 
-    } 
- 
-    public static void main(String[] args) { 
-        Scanner sc = new Scanner(System.in); 
-        String text = sc.next(); 
-        
-        int key = sc.nextInt(); 
-        String encrypted = encrypt(text, key); 
-        System.out.println("Encrypted Text:" + encrypted); 
-        System.out.print("\n"); 
-        String decrypted = decrypt(encrypted, key); 
-        System.out.println("Decrypted Text:" + decrypted); 
-    } 
-} 
+        System.out.println(); 
+    }
+    
+    StringBuilder decryptedtext = new StringBuilder();
+    int row1 = 0;
+    boolean movedown1 = false;
+    for(int i = 0 ; i < ciphertext.length() ; i++){
+      if(rail[row1][i] != '.'){
+        decryptedtext.append(rail[row1][i]);
+      }
+      if(row1 == 0 || row1 == key - 1){
+        movedown1 = !movedown1;
+      }
+      row1 += movedown1 ? 1 : -1;
+    }
+    
+    String decryptedstring = decryptedtext.toString();
+    return decryptedstring;
+  }
+  public static void main(String []args){
+    String ciphertext = encrypt("HELLOWORLD", 3);
+    System.out.println("Cipher: " + ciphertext);
+    String decryptedtext = decrypt("HOLELWRDLO", 3);
+    System.out.println("Decrypted Message: " + decryptedtext);
+  }
+}
