36e37ed376e5691b86cd7ad8dbb7d566d4a1255a
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2026-05-07 10:31:34 +0530

    Create lis.java

---
+++ stack_linkedlist_queue_codes_for_sts/lis.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args){
+    int []arr = {8, 17, 6, 25, 15, 45, 41, 54};
+    int n = arr.length;
+    int []lis = new int[n+1];
+    lis[0] = 1;
+    int max = 1;
+    for(int i = 1 ; i < n ; i++){
+      if(arr[i] > arr[i-1]){
+        lis[i] = max + 1;
+        if(lis[i] > max) max = lis[i];
+      }
+      else{
+        lis[i] = lis[i-1]-1;
+      }
+    }
+    for(int x : lis){
+      System.out.print(x + " ");
+    }
+    System.out.print("\nMax subseq: " + lis[n-1]);
+  }
+}
